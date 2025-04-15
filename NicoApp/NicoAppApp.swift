ee8b9e98ca12bd74646d0a996bef5acd81d02665
Nicolas Diaz <nicolasdiazoff@gmail.com>
2025-04-14 23:55:01 -0500

    Initial Commit

---
+++ NicoApp/NicoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NicoAppApp.swift
+//  NicoApp
+//
+//  Created by Nicolás Díaz on 14/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NicoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NicoAppTests/NicoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NicoAppTests.swift
+//  NicoAppTests
+//
+//  Created by Nicolás Díaz on 14/4/25.
+//
+
+import Testing
+@testable import NicoApp
+
+struct NicoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
